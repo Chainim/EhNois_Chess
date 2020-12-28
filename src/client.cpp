6f1c8527b57c0f41c54a673a4754aef75ec46f1f
unknown <cirilo2002@live.com>
2020-12-28 17:23:16 -0300

    sarve

--- src/client.cpp
+++ src/client.cpp
@@ -7,5 +7,11 @@ int main()
 	SOCKET servidor;
 	connect(servidor, "127.0.0.1", 7777);
 
+	char recebe_lado;
+	recebe_lado = '0';
+	recv(servidor,&recebe_lado,1,0);
+
+	printf("lado %c\n",recebe_lado);
+
 	network_shutdown();
 }
--- src/server.cpp
+++ src/server.cpp
@@ -9,5 +9,10 @@ int main()
 	client1 = accept(listen_socket, nullptr, nullptr);
 	client2 = accept(listen_socket, nullptr, nullptr);
 
+	char decide_lado = '1';
+	send(client1,&decide_lado,1,0);
+	decide_lado = '2';
+	send(client2,&decide_lado,1,0);
+
 	network_shutdown();
 }
